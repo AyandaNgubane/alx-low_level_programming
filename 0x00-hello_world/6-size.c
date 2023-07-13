78bf1e5efdfeb230b3cb0989797679877991fecb
Ayanda Ngubane <ayanda4me@gmail.com>
2023-07-13 01:31:01 -0700

    6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ * 
+ * This program prints out the size of varios data types
+ *
+ * Return : should be 0 if successful
+ */
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
