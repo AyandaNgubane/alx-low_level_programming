c6624fa0451744c4fde0fd49f697572e40030ad7
Ayanda Ngubane <ayanda4me@gmail.com>
2023-07-13 00:48:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * This program that prints out a string using the puts() function
+ * it should return zero if passed
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
