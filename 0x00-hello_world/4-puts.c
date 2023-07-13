86df30c58de83d9a078f7f26c0ef211fdd5d67da
Ayanda Ngubane <ayanda4me@gmail.com>
2023-07-13 01:07:43 -0700

    4-puts.c - fix2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * This program that prints out a string using the puts() function
- *
  * main - Starting point
+ *
+ * This program prints out a string
  *
  * Return: 0 if successful
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
