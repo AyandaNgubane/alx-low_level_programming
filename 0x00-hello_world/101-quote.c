828156330e202726e543a6c90c4218f3f26cfae8
Ayanda Ngubane <ayanda4me@gmail.com>
2023-07-13 04:33:06 -0700

    101-quote.c - fix3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
- * main - aprogram that prints out to stderr
+/**
+ * main - a program that prints out to stderr
  *
  * Return: 1 if successfull
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
