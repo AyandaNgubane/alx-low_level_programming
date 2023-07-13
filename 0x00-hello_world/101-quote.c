c2f18339f8a9086b24510fe46fd43320806e2f06
Ayanda Ngubane <ayanda4me@gmail.com>
2023-07-13 04:29:39 -0700

    101-quote.c - fix2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /*
  * main - aprogram that prints out to stderr
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-	char str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";, sizeof(str));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
