f57e6d346ea12402d6827f4fd6b12ec12385f47b
Ayanda Ngubane <ayanda4me@gmail.com>
2023-07-13 01:15:04 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ *
+ * This program prints out a string
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
