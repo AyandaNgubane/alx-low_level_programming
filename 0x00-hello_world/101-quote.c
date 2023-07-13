2702b8b66eb891909d8eaa63f6845130c0fc232d
Ayanda Ngubane <ayanda4me@gmail.com>
2023-07-13 04:19:34 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main - aprogram that prints out to stderr
+ *
+ * Return: 1 if successfull
+ */
+int main(void)
+{
+	char str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, str\n, sizeof(str));
+	return (1);
+}
