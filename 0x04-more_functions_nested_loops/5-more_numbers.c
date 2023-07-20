96767d76a2783792c1346acaa737f47284a8d665
Ayanda Ngubane <ayanda4me@gmail.com>
2023-07-20 06:10:23 -0700

    3 4 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_more_numbers - a function that prints 10 times the numbers, from 0 to 14, followed by a new line
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14, followed by a new line
  *
  */
-void print_more_numbers(void)
+void more_numbers(void)
 {
 	int a;
 
