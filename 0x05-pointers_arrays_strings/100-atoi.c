1da29f0b5d377743569c56a373c606273a373a3a
Ayanda Ngubane <ayanda4me@gmail.com>
2023-07-24 12:43:25 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,21 +12,19 @@ int _atoi(char *s)
 	int neg;
 	int pos;
 	unsigned int dig;
+	int sign;
 
 	neg = 0;
 	pos = 0;
 	dig = 0;
 	i = 0;
+	sign = 1;
 	
 	while (!((s[i] >= '0') && (s[i] <= '9')) && (s[i] != '\0'))
 	{
 		if (s[i] == '-')
 		{
-			neg++;
-		}
-		else if (s[i] == '+')
-		{
-			pos++;
+			sign = -(sign);
 		}
 		i++;
 	}
@@ -35,13 +33,6 @@ int _atoi(char *s)
 		dig = (dig * 10) + (s[i] - '0');
 		i++;
 	}
-	if (neg > pos)
-	{
-		dig = -(dig);
-	}
-	else
-	{
-		dig = dig;
-	}
+	dig = dig * sign;
 	return (dig);
 }
