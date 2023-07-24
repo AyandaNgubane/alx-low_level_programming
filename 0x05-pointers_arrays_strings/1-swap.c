fc266b3af97a9538f0005c682671722617f6d393
Ayanda Ngubane <ayanda4me@gmail.com>
2023-07-24 00:30:23 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of 2 integers
+ *
+ * @a: variable to be swapped
+ * @b: variable to be swapped
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
