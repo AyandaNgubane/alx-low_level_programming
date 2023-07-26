d406c07226f3c5ebf7775d109e6c7b82c7bf7600
Ayanda Ngubane <ayanda4me@gmail.com>
2023-07-26 04:14:39 -0700

    Task 0 - Task 3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: destination
+ * @src: string to copy
+ * @n: number of bytes
+ * Return: the full string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i]
+		i++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ * Return: result
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int result;
+
+	result = 0;
+
+	for (i = 0; (s1[i] != '\0') || (s2[i] != '\0'); i++)
+	{
+		if (s1[i] > s2[i])
+		{
+			result = s1[i] - s2[i];
+			break;
+		}
+		else if (s1[i] < s2[i])
+		{
+			result = s1[i] - s2[i];
+			break
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return (result);
+}
