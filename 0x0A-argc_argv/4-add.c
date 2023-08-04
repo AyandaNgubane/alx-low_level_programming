67e5479f5561692b75d883347a9a61371a88a5fe
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-04 06:15:30 -0700

    Errors

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,48 +3,6 @@
 #include "main.h"
 
 /**
- * main - adds all arguments arguments
- *
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 if successful and 1 for an error
- */
-int main(int argc, char *argv[])
-{
-	int result;
-	int i;
-	int r;
-	int t;
-	char *c;
-	char *d;
-
-	result = 0;
-	c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	/*d = " !@#$%^&*()_+=-}{][|:|\\,./?><";*/
-
-	if (argc < 1)
-	{
-		printf("0\n");
-		return (0);
-	}
-	for (i = 0; i < argc; i++)
-	{
-		r = _strpbrk(argv[i], c);
-		t = _strpbrk(argv[i], d);
-		if (r != 0 && t != 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			result = result + atoi(argv[i]);
-		}
-	}
-	printf("%d\n", result);
-	return (0);
-}
-/**
  * _strpbrk - similar function to strpbrk
  *
  * @s: string to check from
@@ -68,4 +26,46 @@ char *_strpbrk(char *s, char *accept)
 	}
 end:
 	return (s[i] != '\0' ? s + i : '\0');
+}
+/**
+ * main - adds all arguments arguments
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful and 1 for an error
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+	int i;
+	int r;
+	int t;
+	char *c;
+	char *d;
+
+	result = 0;
+	c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	d = " !@#$%^&*()_+=-}{][|:|\\,./?><";
+
+	if (argc < 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		r = _strpbrk(argv[i], c);
+		t = _strpbrk(argv[i], d);
+		if (r != 0 && t != 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			result = result + atoi(argv[i]);
+		}
+	}
+	printf("%d\n", result);
+	return (0);
 }
