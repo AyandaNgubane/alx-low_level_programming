f71610f91008061920062a8b7a91b05f33de474a
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-10 09:24:09 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,39 +17,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	l1 = 0, l2 = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	for (i = 0; s2[i] != '\0'; i++)
-	{
 		l2++;
-	}
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		l1++;
-	}
 	if (n >= l2)
-	{
 		n = l2;
-	}
 	if (n == 0)
-	{
 		return (s1);
-	}
-
 	s = malloc((sizeof(char) * l1) + (sizeof(char) * n) + 1);
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		s[i] = s1[i];
-	}
 	for (j = 0; j < n; j++)
 	{
 		s[i] = s2[j];
