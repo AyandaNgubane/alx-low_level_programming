5ae129f2e10e0e2ef4faea6c01d841f0da1755cd
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-14 06:14:45 -0700

    Task 0 - Task 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,9 +14,14 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - dog structure
+ */
+typedef struct dog dog_t;
 #endif
 #ifndef _FUNCTIONS_H
 #define _FUNCTIONS_H
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
