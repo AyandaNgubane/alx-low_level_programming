b2367e11118363f3764ecdc93f103d87fa261241
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-17 06:25:34 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,7 +16,7 @@ void print_all(const char * const format, ...)
 	char *str;
 	int length;
 
-	length = strlen(format);
+	length = _strlen(format);
 	va_start(args, format);
 	i = 0;
 
@@ -51,4 +51,23 @@ void print_all(const char * const format, ...)
 	}
 	va_end(args);
 	printf("\n");
+}
+/**
+ * _strlen - counts length of string
+ *
+ * @s: string to be checked
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int i;
+	int length;
+
+	length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
 }
