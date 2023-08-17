9d2f312042f3e27f6c763139778adbd90dc97217
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-17 03:20:31 -0700

    Task 0 - Task 2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,21 +9,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
-
 	va_list args;
 	unsigned int i;
 	int j, sum;
+
 	va_start(args, n);
 
 	sum = 0;
-
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		j = va_arg(args, int);
-		sum = sum + (j);
+		for (i = 0; i < n; i++)
+		{
+			j = va_arg(args, int);
+			sum = sum + (j);
+		}
+		va_end(args);
 	}
-	va_end(args);
 	return (sum);
 }
