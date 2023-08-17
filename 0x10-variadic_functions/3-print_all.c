ba53114b415b66f9f7ae6ef08c16187b52e6c693
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-17 11:20:18 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,7 +19,7 @@ void print_all(const char * const format, ...)
 	va_start(args, format);
 	i = 0;
 
-	while (i < length && format && format[0] != '\0')
+	while (i < length && format != NULL && format[0] != '\0')
 	{
 		switch (format[i])
 		{
