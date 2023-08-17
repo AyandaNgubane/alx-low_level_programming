0b9cf686007ba3bc6757dc616039e3467b874c0b
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-17 04:28:28 -0700

    Task 0 - Task 2

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,7 @@
+#ifndef _HEADER_FILE_
+#define _HEADER_FILE_
+
 int sum_them_all(const unsigned int n, ...);int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+#endif
