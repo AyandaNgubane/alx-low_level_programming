23b3a03c776bc32fb9e3cf1c7ac797991451174e
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-24 12:06:05 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
@@ -17,8 +17,8 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
-		nodes++;
+		count++;
 	}
 
-	return (nodes);
+	return (count);
 }
