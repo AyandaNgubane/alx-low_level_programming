99d1353224c4cb6cbf7dff928172f9b0e0806dd7
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-24 12:44:04 -0700

    Advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * starting_point - runs before main
+ * Return: void.
+ */
+
+void __attribute__ ((constructor)) starting_point(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
