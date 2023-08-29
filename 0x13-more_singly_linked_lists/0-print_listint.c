c68a8892030f09782a9b50005988c91f6e768bbd
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-29 12:44:26 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,16 +7,18 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *cursor = h;
 	size_t nodes;
 
 	nodes = 0;
 
-	while (cursor != NULL)
+	if (h != NULL)
 	{
-		printf("%d\n", cursor->n);
-		nodes++;
-		cursor = cursor->next;
+		while (h)
+		{
+			printf("%d\n", h->n);
+			nodes++;
+			h = h->next;
+		}
 	}
 	return (nodes);
 }
