3b3a56df06ee49c5a06e676e2419604263016297
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-29 10:11:29 -0700

    Task 14

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
 size_t free_listint_safe(listint_t **h)
 {
 	size_t nodes = 0;
