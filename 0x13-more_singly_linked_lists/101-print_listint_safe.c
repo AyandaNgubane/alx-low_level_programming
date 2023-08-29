e504a00c563e81756a115900a3e034b0144d4f55
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-29 09:15:52 -0700

    Task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes = 0;
+	listp_t *new, *xtr, pnt;
+
+	pnt = NULL;
+	while (head != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = pnt;
+		pnt = new;
+
+		xtr = pnt;
+
+		while (xtr->next != NULL)
+		{
+			xtr = xtr->next;
+			if (head == xtr->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				_free(&pnt);
+				return (nodes);
+			}
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nodes++;
+	}
+
+	_free(&pnt);
+	return (nodes);
+}
+/**
+ * _free - frees a linked list
+ * @head: head of a list
+ *
+ * Return: void
+ */
+void _free(listp_t **head)
+{
+	listp_t *new, *tmp;
+
+	if (head != NULL)
+	{
+		new = *head;
+		while ((new = tmp) != NULL)
+		{
+			new = new->next;
+			free(tmp);
+		}
+		*head = NULL;
+	}
+}
