55a3a19a3307dafd638bc88df1b510e2903e8f76
Ayanda Ngubane <ayanda4me@gmail.com>
2023-09-04 05:34:03 -0700

    Task 2 and Task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * main - starting point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_file(argv[1], argv[2]);
+	exit(0);
+}
+
+/**
+  * file_cpy - copies file content into another
+  * @orig: file to copy from
+  * @cpy: file to copy to
+  * Return: void
+  */
+void file_cpy(const char *orig, const char *cpy)
+{
+	int fd_orig, fd_cpy, len_r;
+	char buffer[1024];
+
+	fd_orig = open(orig, O_RDONLY);
+	if (orig == NULL || fd_orig == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", orig);
+		exit(98);
+	}
+
+	fd_cpy = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((len_r = read(fd_orig, buffer, 1024)) > 0)
+	{
+		if (fd_cpy == -1 || write(fd_cpy, buffer, len_r) != len_r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", cpy);
+			exit(99);
+		}
+	}
+
+	if (len_r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", orig);
+		exit(98);
+	}
+
+	if (close(fd_orig) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_orig);
+		exit(100);
+	}
+
+	if (close(fd_cpy) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_cpy);
+		exit(100);
+	}
+}
